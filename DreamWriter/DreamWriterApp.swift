492ebd811e6db4eead09e8e8d47cf69326aaca2b
Matt McDonnell <matt.mcdonnell@nasa.gov>
2024-12-07 21:17:20 -0600

    Adding remote

---
+++ DreamWriter/DreamWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWriterApp.swift
+//  DreamWriter
+//
+//  Created by Matthew McDonnell on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DreamWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DreamWriterTests/DreamWriterTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWriterTests.swift
+//  DreamWriterTests
+//
+//  Created by Matthew McDonnell on 12/7/24.
+//
+
+import Testing
+@testable import DreamWriter
+
+struct DreamWriterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
